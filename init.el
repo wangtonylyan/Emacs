e32f60d417a2d7ce94d6a263fcdf9419b95d351d
TonyLYan <wangtonylyan@outlook.com>
2015-06-16 10:00:15 +0800

    v0

---
+++ init.el
@@ -0,0 +1,207 @@
+;; Emacs initialization file
+; ~/.emacs > ~/.emacs.el > ~/.emacs.d/init.el
+;1)加载插件的方式
+;load：每次调用时都会（重新）加载
+;require：若未加载，则加载之，而不会重新加载
+;autoload：使用时才require
+;eval-after-load：在一次load操作之后且在执行该loaded file/feature中的脚本之前
+;例如：eval-after-load中还"来得及"注册某个模式的hook
+;其第二个参数必须为quote form，否则的话会在作为传参时被立即计算
+;2)目前所采取的策略
+;在Emacs启动时load所有配置文件
+;配置文件中的函数定义在load时一同evaluate
+;插件的初始化工作使用eval-after-load，或注册于诸如c-initialization-hook的hook中，随对应模式的首次启动而执行其初始化工作
+;全局性插件的启动紧随其自身的初始化完成之后，局部性插件的启动则随对应模式的启动
+
+
+;===========================================================================
+; Windows
+;===========================================================================
+; 1）在C:\Users\用户名\AppData\Roaming\.emacs文件中指示导入本文件
+;(load-file "E:/home/wm/.emacs.d/init.el")
+; 2）以下是本文件的内容
+; 设置Emacs内部的环境变量，此方式需要每次在Emacs启动时被重新设置，不会保存
+; 也可以设置Windows系统的环境变量，但并不推荐，因为会影响到其他同样依赖于该变量的软件
+(when (eq system-type 'windows-nt)
+  (setenv "HOME" "E:/")
+  ; 设置默认工作目录
+  (setq default-directory "~/")
+  ; 设置shell，默认使用cmdproxy.exe
+  (setq shell-file-name "C:/windows/system32/cmd.exe")
+  (setq shell-command-switch "/c")
+  )
+
+
+;===========================================================================
+; Execution Path
+;===========================================================================
+;; 配置Emacs的exec-path就无需修改操作系统的环境变量了
+;; 当然也可以设置(setenv "PATH" "")，效果是一样的
+(setq my-emacs-exec-bin-path "D:/softwares/")
+(add-to-list 'exec-path my-emacs-exec-bin-path)
+
+
+;===========================================================================
+; 下述内容会因用户在Customize界面的保存操作而由Emacs自动写入
+;===========================================================================
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(current-language-environment "Chinese-GB")
+ '(tool-bar-mode nil) ;取消工具栏
+ '(global-font-lock-mode nil) ;取消全局性的语法高亮
+ '(electric-indent-mode nil) ;Emacs24.5以上版本会默认启用该模式
+ '(ecb-options-version "2.40") ;ecb-minor-mode
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(highlight-changes ((t (:background "#CCFFCC"))))
+ '(highlight-changes-delete ((t (:background "#CCFFCC"))))
+ )
+
+
+;===========================================================================
+; 配置杂项
+;===========================================================================
+;; Account
+(setq user-full-name "Tony")
+(setq user-mail-address "wangtonylyan@outlook.com")
+
+;; UI
+;(setq inhibit-startup-message 1) ;取消启动界面
+(setq frame-title-format "emacs@%b") ;设置标题栏显示为buffer名字
+;; 关于smooth scrolling可以参考
+;; http://www.emacswiki.org/emacs/SmoothScrolling
+;; 其提供的主要解决方案是基于两个插件：smooth-scroll.el和smooth-scrolling.el
+;; 这里暂不使用平滑滚动，而是通过设置以下变量以尽可能地避免页面滚动时画面的频繁跳动
+(setq redisplay-dont-pause t
+      ;; mouse wheel scrolling
+      mouse-wheel-scroll-amount '(3 ((shift) . 1))
+      mouse-wheel-progressive-speed nil
+      mouse-wheel-follow-mouse t
+      ;; keyboard scrolling
+      scroll-margin 1
+      scroll-step 3
+      scroll-conservatively 10000
+      scroll-preserve-screen-position 1
+      )
+(setq-default scroll-up-aggressively 0.01
+              scroll-down-aggressively 0.01)
+(mouse-avoidance-mode 'animate) ;当光标移动至鼠标位置时，为避免遮挡视线，自动移开鼠标
+(setq-default line-spacing 0) ;行距
+(column-number-mode 1) ;在mode-line显示列数
+(set-face-background 'default "#C7EDCC") ;设置背景颜色
+(set-face-attribute 'default nil :family "Monaco" :weight 'semi-bold) ;设置字体
+(set-default-font "13") ;设置字号
+(show-paren-mode 1) ;左右括号相匹配显示
+(setq show-paren-style 'parentheses)
+(setq debug-on-error t) ;显示错误信息
+(fset 'yes-or-no-p 'y-or-n-p) ;以y/n替换yes/no
+(setq visible-bell t) ;以窗口闪烁的方式代替错误提示音
+
+;; Edit
+(setq-default indent-tabs-mode nil) ;make indentation commands use space only
+(setq-default tab-width 4)
+;(setq tab-always-indent t)
+(global-highlight-changes-mode 1)
+(setq highlight-changes-global-changes-existing-buffers nil)
+(setq highlight-changes-visibility-initial-state t)
+(setq highlight-changes-face-list nil)
+(setq highlight-changes-colors nil)
+; 每次保存buffer时都将删除现有的改动高亮
+; 替换成这两个hook就会无效，原因未知：write-content-functions或write-file-functions
+(add-hook 'before-save-hook
+          (lambda ()
+            (delete-trailing-whitespace) ;删除每行末尾的空格
+            (highlight-changes-remove-highlight (point-min) (point-max))
+            ))
+
+;; Backup
+(setq make-backup-files t) ;启用自动备份
+(setq version-control t) ;启用版本控制，即可以备份多次
+(setq kept-old-versions 1) ;备份最旧的版本个数
+(setq kept-new-versions 1) ;备份最新的版本个数
+(setq delete-old-versions t)
+(setq dired-kept-versions 2)
+(setq backup-directory-alist '(("." . "~/.emacs.d.backups"))) ;设置备份文件的路径
+(setq backup-by-copying t) ;备份设置方式为直接拷贝
+
+;; File Extension
+;(setq auto-mode-alist
+;      (cons '("\\.emacs\\'" . emacs-lisp-mode) auto-mode-alist))
+
+;; Key
+;(global-set-key "\C-c s" 'func)
+;(define-key lisp-mode (kbd "C-c ;") 'func)
+
+;; Mode
+(icomplete-mode 1) ;当在minibuffer中输入时，提供命令补全功能
+;(ido-mode 1) ;当在minibuffer中输入时，提供文件名补全功能
+;(uniquify-mode 1) ;buffer命名
+;; built-in Speedbar (rather than CEDET Speedbar)
+(setq speedbar-use-images nil) ;不使用image方式
+(setq speedbar-show-unknown-files t)
+
+
+;===========================================================================
+; 加载全局性的第三方插件
+;===========================================================================
+;; 第三方插件资源：
+;; https://github.com/emacsmirror
+
+(setq my-emacs-config-file-path "~/.emacs.d/my-emacs/")
+(setq my-emacs-plugin-load-path "~/.emacs.d/site-lisp/")
+(add-to-list 'load-path my-emacs-plugin-load-path)
+
+;===========================================================================
+; Color Theme
+;===========================================================================
+; version: 6.6.0
+; http://www.nongnu.org/color-theme/
+; 该插件已集成于Emacs24以上版本中，作为默认的主题管理组件
+; 其自带了一些主题，网上还有许多基于该插件独立发布的主题
+;===========================================================================
+(setq my-emacs-theme-load-path "~/.emacs.d/site-lisp/color-theme/")
+(add-to-list 'custom-theme-load-path my-emacs-theme-load-path)
+;---------------------------------------------------------------------------
+; [theme] Solarized
+;---------------------------------------------------------------------------
+; http://ethanschoonover.com/solarized
+; https://github.com/altercation/solarized
+; https://github.com/sellout/emacs-color-theme-solarized
+; 该主题已支持各种平台，包括了Emacs、Vim、VisualStudio
+; 下载下来后只需保留三个.el脚本文件即可：
+; color-theme-solarized.el, solarized-theme.el, solarized-definitions.el
+;===========================================================================
+;(setq my-if-use-solarized t)
+(when (and (boundp 'my-if-use-solarized)
+           my-if-use-solarized)
+  (progn
+    (load-theme 'solarized t)
+    (add-hook 'after-make-frame-functions
+              (lambda (frame)
+                (let ((mode 'dark)) ;'dark or 'light
+                  (set-frame-parameter frame 'background-mode mode)
+                  (set-terminal-parameter frame 'background-mode mode))
+                (enable-theme 'solarized)))
+    ))
+
+
+;===========================================================================
+; 加载其他配置文件
+;===========================================================================
+; prog-mode
+(load-file (concat my-emacs-config-file-path "prog.emacs"))
+; cc-mode (c-mode, c++-mode, java-mode)
+(load-file (concat my-emacs-config-file-path "prog-cc.emacs"))
+; lisp-mode, emacs-lisp-mode
+(load-file (concat my-emacs-config-file-path "prog-lisp.emacs"))
+; python-mode
+(load-file (concat my-emacs-config-file-path "prog-py.emacs"))
+; web-browser
+(load-file (concat my-emacs-config-file-path "web-browser.emacs"))
---
+++ my-emacs/web-browser.emacs
@@ -0,0 +1,43 @@
+;===========================================================================
+; Text-Based Web Browser
+;===========================================================================
+; 此类网页浏览器被称为文本或终端浏览器，适用于在文本编辑器或终端中使用，最主要的就是以下三者
+; 1)w3m
+; version: 0.5.3
+; http://w3m.sourceforge.net/
+; 官网提供的是源码，可以编译安装；对于Windows平台而言，可以使用由网友提供的.exe可执行程序
+; 在Emacs中可与插件emacs-w3m配合使用
+; 2)Lynx
+; http://lynx.browser.org/
+; 3)Links
+; http://www.jikos.cz/~mikulas/links/
+
+
+;===========================================================================
+; emacs-w3m
+;===========================================================================
+; http://emacs-w3m.namazu.org/
+; https://github.com/emacsmirror/w3m
+; 此插件作为Emacs使用w3m浏览器的接口
+; 目前官网上提供下载的发布版v1.4.4不支持Emacs23以上版本，需要下载最新开发版
+; 但由于是开发版本因此需经常关注，以实时跟进从而避免bug
+; 1)从CVS上下载
+; http://cvs.namazu.org/Development/emacs-w3m/
+; [shell]$ cvs -d :pserver:anonymous@cvs.namazu.org:/storage/cvsroot co emacs-w3m
+; 或下载以下由网友提供的github镜像
+; https://github.com/doitian/emacs-w3m
+; 2)安装该插件之前需先完成w3m的安装，并将其可执行程序所在路径加入到PATH中
+; [shell]$ emacs -Q -l w3mhack.el NONE -f w3mhack-nonunix-install
+;===========================================================================
+(defun my-plugin-w3m-init ()
+  (add-to-list 'exec-path (concat my-emacs-exec-bin-path "w3m"))
+  (add-to-list 'load-path (concat my-emacs-plugin-load-path "emacs-w3m"))
+  (require 'w3m-load)
+  (setq w3m-home-page "http://www.baidu.com/")
+  (setq w3m-use-cookies t)
+  (setq w3m-use-favicon nil)
+  )
+
+(defun my-plugin-w3m-start ()
+  (w3m-mode)
+  )
