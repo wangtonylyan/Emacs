dd372f9ca5764404bb1e901a8155322189b78531
TonyLYan <wangtonylyan@outlook.com>
2017-05-21 18:17:44 +0800

    optimize basic settings

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+;; 判断Emacs版本可以基于以下两个变量：'emacs-major-version和'emacs-minor-version
+
 (when (eq system-type 'windows-nt)
   (add-to-list 'exec-path "D:/softwares" t)
   (let ((path (executable-find
@@ -21,8 +23,6 @@
 ;; 指定由(customize)写入配置信息的文件，随后每当Emacs自动写入时就不会再修改当前文件了
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file)
-
-;; 判断Emacs版本可以基于以下两个变量：'emacs-major-version和'emacs-minor-version
 
 ;; =============================================================================
 ;; 管理和加载全局性的第三方插件
@@ -56,10 +56,13 @@
 
   ;; 目前使用此全局变量来管理插件的启用/禁用，其中包括了ELPA更新源中所没有的插件
   (setq package-selected-packages '(;; 1) enhancement
+                                    ;; use-package
                                     atom-one-dark-theme
                                     powerline
                                     helm ;; icomplete, anything, ido, smex, ivy
                                     ace-jump-mode
+                                    flyspell
+                                    flyspell-correct-helm
                                     ;; 2) programming
                                     yasnippet
                                     company ;; auto-complete
@@ -112,6 +115,20 @@
   (powerline-default-theme))
 
 ;; =============================================================================
+;; Flyspell
+;; Linux系统中默认使用Aspell作为拼写检查工具，http://aspell.net/
+;; Aspell是Ispell的继承者，但Emacs中仍保留了ispell这一模式名
+(let ((exec (executable-find "aspell")))
+  (when (and exec
+             (member 'flyspell package-selected-packages)
+             (require 'ispell nil t)
+             (require 'flyspell nil t))
+    (setq ispell-program-name exec ;; 设置后台支持程序
+          ;; ispell-dictionary "english" ;; default dictionary
+          ;; ispell-personal-dictionary ""
+          flyspell-issue-message-flag nil)))
+
+;; =============================================================================
 ;; Auctex
 ;; -----------------------------------------------------------------------------
 (when (and (member 'auctex package-selected-packages)
@@ -161,46 +178,33 @@
 ;; =============================================================================
 ;; 配置杂项
 ;; -----------------------------------------------------------------------------
-;; Account
-(setq user-full-name "TonyLYan"
-      user-mail-address "wangtonylyan@outlook.com")
-
-;; UI
-(setq inhibit-startup-message 1) ;; 取消启动界面
-(tool-bar-mode -1) ;; 取消工具栏
-(setq frame-title-format "emacs@%b") ;; 设置标题栏显示为buffer名字
-;; 关于smooth scrolling可以参考: http://www.emacswiki.org/emacs/SmoothScrolling
-;; 其提供的主要解决方案是基于两个插件：smooth-scroll.el和smooth-scrolling.el
-;; 这里暂不使用平滑滚动，而是通过设置以下变量以尽可能地避免页面滚动时画面的频繁跳动
-;; mouse wheel scrolling
-(setq mouse-wheel-scroll-amount '(3 ((shift) . 1))
-      mouse-wheel-progressive-speed nil
-      mouse-wheel-follow-mouse t)
-;; keyboard scrolling
-(setq scroll-margin 1
-      scroll-step 3
-      scroll-conservatively 10000
-      scroll-preserve-screen-position 1)
-(setq-default scroll-up-aggressively 0.01
-              scroll-down-aggressively 0.01)
-(scroll-bar-mode -1) ;; 取消滚动条
-(mouse-avoidance-mode 'animate) ;; 当光标移动至鼠标位置时，为避免遮挡视线，自动移开鼠标
-(setq-default cursor-type '(bar . 3))
-;; (save-place-mode 1) ;; 记录光标在每个文件中最后一次访问时所在的位置
-(global-hl-line-mode 1)
-(setq-default line-spacing 0) ;; 行距
-(column-number-mode 1) ;; 在mode-line显示列数
-;; (setq-default fill-column 80) ;; 超过80字符就换行显示
-
-(setq-default buffer-file-coding-system 'utf-8)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
 (prefer-coding-system 'utf-8)
-
-(show-paren-mode 1) ;; 显示匹配的左右括号
-(setq show-paren-style 'parentheses)
-(setq debug-on-error t) ;; 显示错误信息
-(fset 'yes-or-no-p 'y-or-n-p) ;; 以y/n替换yes/no
-(setq visible-bell t) ;; 以窗口闪烁的方式代替错误提示音
-(global-visual-line-mode -1) ;; 对中文支持不好
+(global-auto-revert-mode 1) ;; 当硬盘上的文件被修改后，Emacs会提示用户重新读取该文件
+(recentf-mode 1)
+(auto-image-file-mode) ;; 允许打开图片
+(auto-compression-mode) ;; 允许查看和写入压缩包
+(defalias 'yes-or-no-p 'y-or-n-p) ;; 以y/n替换yes/no
+(setq user-full-name "TonyLYan"
+      user-mail-address "wangtonylyan@outlook.com"
+      inhibit-startup-message 1 ;; 取消启动界面
+      frame-title-format '(buffer-file-name "%f" ("%b")) ;; 设置标题栏显示为buffer名字
+      uniquify-buffer-name-style 'post-forward-angle-brackets ;; 重名buffer的命名
+      visible-bell t ;; 以窗口闪烁的方式代替错误提示音
+      echo-keystrokes 0.1
+      debug-on-error nil ;; 显示错误信息
+      select-enable-clipboard t
+      delete-by-moving-to-trash t
+      make-backup-files t ;; 启用自动备份
+      version-control t ;; 启用版本控制，即可以备份多次
+      kept-old-versions 1 ;; 备份最旧的版本个数
+      kept-new-versions 1 ;; 备份最新的版本个数
+      delete-old-versions t
+      dired-kept-versions 2
+      backup-directory-alist '(("." . "~/.emacs.d.backups"))
+      backup-by-copying t)
+(setq-default buffer-file-coding-system 'utf-8)
 
 ;; (set-face-background 'default "#C7EDCC") ;; 设置背景颜色为绿色护眼色
 ;; 字体的名字源自于.ttf或.otf文件内自带的元信息，包括family和style等
@@ -219,43 +223,62 @@
 ;; (set-face-attribute 'default nil :family "Microsoft YaHei Mono" :weight 'normal :height 110) ;; 设置字体，包括字号等
 ;; (set-frame-font "10" nil t) ;; 设置字号, 同(set-face-attribute)中的:height
 
-;; Edit
-(electric-indent-mode -1) ;; 自动缩进
+(global-font-lock-mode 1) ;; 语法高亮
+;; (add-hook 'xxx-mode-hook 'turn-on-font-lock) ;; (font-lock-mode 1)
+;; (global-linum-mode 1) ;; 左侧行号，推荐仅将其显示于主要的编辑文档中
+;; (add-hook 'xxx-mode-hook 'linum-mode)
+(global-highlight-changes-mode 1)
+(mouse-avoidance-mode 'animate) ;; 当光标移动至鼠标位置时，为避免遮挡视线，自动移开鼠标
+;; (save-place-mode 1) ;; 记录光标在每个文件中最后一次访问时所在的位置
+(set-cursor-color "white")
+;; (blink-cursor-mode -1)
+(column-number-mode 1) ;; 在mode-line显示列数
+(scroll-bar-mode -1) ;; 取消滚动条
+(global-hl-line-mode 1)
+(global-visual-line-mode -1) ;; 对中文支持不好
+(show-paren-mode 1) ;; 显示匹配的左右括号
 (electric-pair-mode 1)
 (electric-quote-mode -1)
-(setq-default indent-tabs-mode nil ;; make indentation commands use space only
-              tab-width 4)
-;; (setq tab-always-indent t)
-(global-highlight-changes-mode 1)
-(setq highlight-changes-global-changes-existing-buffers nil
+(electric-indent-mode -1) ;; 自动缩进
+(setq font-lock-maximum-decoration t
+      transient-mark-mode t
+      shift-select-mode nil
+      highlight-changes-global-changes-existing-buffers nil
       highlight-changes-visibility-initial-state t
       highlight-changes-face-list nil
-      highlight-changes-colors nil)
+      highlight-changes-colors nil
+      line-move-visual t
+      track-eol t
+      ;; 关于smooth scrolling可以参考: http://www.emacswiki.org/emacs/SmoothScrolling
+      ;; 其提供的主要解决方案是基于两个插件：smooth-scroll.el和smooth-scrolling.el
+      ;; 这里暂不使用平滑滚动，而是通过设置以下变量以尽可能地避免页面滚动时画面的频繁跳动
+      ;; mouse wheel scrolling
+      mouse-wheel-scroll-amount '(3 ((shift) . 1))
+      mouse-wheel-progressive-speed t
+      mouse-wheel-follow-mouse t
+      ;; keyboard scrolling
+      scroll-margin 1
+      scroll-step 3
+      scroll-conservatively 10000
+      scroll-preserve-screen-position 1
+      truncate-partial-width-windows nil
+      require-final-newline t
+      show-paren-style 'parentheses
+      tab-always-indent 'complete)
+(setq-default cursor-type '(bar . 3)
+              scroll-up-aggressively 0.01
+              scroll-down-aggressively 0.01
+              line-spacing 0 ;; 行距
+              ;; fill-column 80 ;; 超过80字符就换行显示
+              indicate-empty-lines nil
+              indent-tabs-mode nil ;; make indentation commands use space only
+              tab-width 4)
 ;; 每次保存buffer时都将删除现有的改动高亮
 ;; 替换成另外两个hook就会无效，原因未知：write-content-functions或write-file-functions
 (add-hook 'before-save-hook
           (lambda ()
             (delete-trailing-whitespace) ;; 删除每行末尾的空格
             (highlight-changes-remove-highlight (point-min) (point-max))))
-(setq require-final-newline t)
-(global-font-lock-mode 1) ;; 语法高亮
-;; (add-hook 'xxx-mode-hook 'turn-on-font-lock) ;; (font-lock-mode 1)
-;; (global-linum-mode 1) ;; 左侧行号，推荐仅将其显示于主要的编辑文档中
-;; (add-hook 'xxx-mode-hook 'linum-mode)
-
-;; Backup and Revert
-(setq make-backup-files t ;; 启用自动备份
-      version-control t ;; 启用版本控制，即可以备份多次
-      kept-old-versions 1 ;; 备份最旧的版本个数
-      kept-new-versions 1 ;; 备份最新的版本个数
-      delete-old-versions t
-      dired-kept-versions 2
-      backup-directory-alist '(("." . "~/.emacs.d.backups")) ;; 设置备份文件的路径
-      backup-by-copying t) ;; 备份设置方式为直接拷贝
-(global-auto-revert-mode 1) ;; 当硬盘上的文件被修改后，Emacs会提示用户重新读取该文件
-
-;; File Extension
-;; (setq auto-mode-alist (cons '("\\.emacs\\'" . emacs-lisp-mode) auto-mode-alist))
 
 (when (not (member 'icomplete package-selected-packages))
   (icomplete-mode -1))
@@ -263,8 +286,9 @@
            (require 'ido nil t))
   (ido-mode 1)
   (ido-everywhere -1) ;; 仅使ido支持find-file和switch-to-buffer
-  (setq ido-enable-flex-matching t
-        ido-enable-prefix t
+  (setq ido-enable-prefix t
+        ido-enable-flex-matching t
+        ido-use-filename-at-point t
         ido-enter-matching-directory nil))
 (when (and (member 'smex package-selected-packages)
            (require 'smex nil t))
@@ -305,8 +329,6 @@
   (helm-autoresize-mode 1)
   (helm-mode 1))
 
-;; uniquify-mode可以为重名的buffer命名
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
 ;; built-in Speedbar (rather than CEDET Speedbar)
 (setq speedbar-use-images nil ;; 不使用image方式
       speedbar-show-unknown-files t)
@@ -329,8 +351,8 @@
 (put 'upcase-region 'disabled nil)
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-c a") 'org-agenda)
-
-(global-unset-key (kbd "C-q"))
+(global-set-key (kbd "C-q") 'read-only-mode)
+(global-unset-key (kbd "C-x C-q"))
 ;; 与输入法切换键冲突
 ;; (global-set-key (kbd "C-S-SPC") 'set-mark-command)
 ;; (global-unset-key (kbd "C-SPC"))
@@ -344,12 +366,14 @@
   (global-set-key (kbd "C-c w") 'ace-jump-word-mode)
   (global-set-key (kbd "C-c l") 'ace-jump-line-mode))
 
+;; File Extension
+;; (setq auto-mode-alist (cons '("\\.emacs\\'" . emacs-lisp-mode) auto-mode-alist))
+
 ;; 调整窗口大小
 ((lambda ()
    ;; 全屏
    ;; (interactive)
-   ;; (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-   ;; '(2 "_NET_WM_STATE_FULLSCREEN" 0))
+   ;; (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_FULLSCREEN" 0))
    ;; 窗口最大化需要分别经过水平和垂直两个方向的最大化
    (interactive)
    (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
