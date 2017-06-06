b7b44396b745a14da807cc09e755662c2d8874d1
wangtonylyan <wangtonylyan@outlook.com>
2017-06-06 15:44:15 +0800

    update cc-style, finish config prog-cc

--- my-emacs/prog-cc.el
+++ my-emacs/prog-cc.el
@@ -1,4 +1,5 @@
 ;; -*- coding: utf-8 -*-
+;; 本文件中的设置仅针对于C和C++，未必适用于Objective-C, Java等
 
 (require 'my-prog)
 
@@ -16,24 +17,90 @@
                 (auto-revert-notify-add-watch)))))
 
 ;; =============================================================================
-;; Style
-;; -----------------------------------------------------------------------------
-(defun my-cc-style-init ()
-  (load-file (concat my-user-emacs-directory "prog-cc-style.el"))
-  (define-key c-mode-base-map "\C-m" 'c-context-line-break) ;; 换行后自动缩进
-  )
+;; CC-mode
+(defun my-plugin-cc-mode-init ()
+  (use-package cc-mode
+    :init
+    (add-hook 'my-prog-cc-mode-start-hook 'my-plugin-cc-mode-start t)
+    :config
+    (c-add-style "my-c-style"
+                 '("stroustrup"
+                   (c-basic-offset . 4)
+                   (c-offsets-alist . ((substatement-open . 0)))
+                   ;; (c-indent-comment-alist . )
+                   ;; (c-indent-comments-syntactically-p . )
+                   ;; (c-comment-only-line-offset . )
+                   ;; (c-special-indent-hook . ())
+                   ;; (c-label-minimum-indentation . )
+                   (c-tab-always-indent . t)
+                   (c-echo-syntactic-information-p . t)
+                   (c-report-syntactic-errors . t)
+                   (c-comment-prefix-regexp . ((c-mode . "//+\\|\\**")
+                                               (c++-mode . "//+\\|\\**")))
+                   (c-block-comment-prefix . "*") ;; 设置block comment在换行时自动添加的前缀
+                   (c-ignore-auto-fill . (string cpp code)) ;; 设置在哪些地方禁用auto-fill功能，目前仅在注释中启用
+                   ;; 设置大括号的前后是否应换行，即"{"将位于行头或行尾或单行或任意位置
+                   ;; 不在以下alist中列出的syntactic symbol，将执行默认行为：(before after)
+                   (c-hanging-braces-alist . ((defun-open)
+                                              (defun-close)
+                                              (class-open)
+                                              (class-close)
+                                              (inline-open)
+                                              (inline-close)
+                                              (block-open)
+                                              (block-close           . c-snug-do-while)
+                                              (statement-cont)
+                                              (substatement-open     . after)
+                                              (statement-case-open   . (before after))
+                                              (brace-list-open)
+                                              (brace-list-close)
+                                              (brace-list-intro)
+                                              (brace-entry-open)
+                                              (extern-lang-open      . after)
+                                              (extern-lang-close     . before)
+                                              (namespace-open        . after)
+                                              (namespace-close       . before)
+                                              (module-open           . after)
+                                              (module-close          . before)
+                                              (composition-open      . after)
+                                              (composition-close     . before)
+                                              (inexpr-class-open     . after)
+                                              (inexpr-class-close    . before)
+                                              (arglist-cont-nonempty)))
+                   ;; 设置冒号的前后是否应换行
+                   ;; 不在以下alist中列出的syntactic symbol，将执行默认行为：()
+                   (c-hanging-colons-alist . ((case-label)
+                                              (label        . after)
+                                              (access-label . after)))
+                   ;; 设置分号和逗号的前后是否应换行
+                   (c-hanging-semi&comma-criteria . (c-semi&comma-no-newlines-before-nonblanks
+                                                     c-semi&comma-inside-parenlist
+                                                     c-semi&comma-no-newlines-for-oneline-inliners))
+                   ;; 清理whitespace，并作为上述hanging的补充(在其后生效)
+                   (c-cleanup-list . (brace-else-brace
+                                      brace-elseif-brace
+                                      brace-catch-brace
+                                      ;; empty-defun-braces
+                                      defun-close-semi
+                                      list-close-comma
+                                      scope-operator
+                                      comment-close-slash))
+                   (c-backslash-column . 48)
+                   (c-backslash-max-column . 72)
+                   (c-auto-align-backslashes . t)))
+    (bind-keys :map c-mode-base-map
+               ;; 换行后自动缩进
+               ("<return>" . c-context-line-break))))
 
-(defun my-cc-style-start ()
-  (c-set-style "my-c-style") ;; 也可以通过(setq c-default-style)实现
+(defun my-plugin-cc-mode-start ()
+  (c-set-style "my-c-style" nil) ;; 也可以通过(setq c-default-style)实现
   (c-toggle-syntactic-indentation 1) ;; 启用根据语法缩进，否则任何基于语法的style都将失效
   (c-toggle-auto-newline 1) ;; 启用auto newline
   (c-toggle-electric-state 1) ;; 按下某些符号如semicolon后自动格式化当前行
-  ;; (read-only-mode 1) ;; 不推荐在这里启用只读模式，会与其他插件冲突
   )
 
 ;; =============================================================================
 ;; CEDET
-;; 以下设置仅针对于C和C++
 (defun my-plugin-cedet-init()
   (use-package cedet
     :demand t
@@ -298,7 +365,7 @@
 ;; =============================================================================
 ;; =============================================================================
 (defun my-prog-cc-mode-init ()
-  (my-cc-style-init)
+  (my-plugin-cc-mode-init)
   ;; 所有依赖于CEDET的插件都必须在CEDET之后被加载/启用
   ;; 否则其会自动加载/启用CEDET，导致上述对于CEDET的设置失效
   ;; 所有与CEDET相互关联的插件的加载/启动顺序将在此被显示地指定
