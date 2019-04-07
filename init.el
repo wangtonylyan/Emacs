55dd18808b2a85f79346bda152398b6acfba2280
TonyLYan <wangtonylyan@outlook.com>
2019-04-07 20:37:53 +0800

    update config

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
-(let ((def-threshold gc-cons-threshold))
+(let ((default gc-cons-threshold))
   ;; this makes garbage collection less frequent,
   ;; which speeds up init by about 0.5 seconds.
   (setq gc-cons-threshold 80000000)
   (load "~/.emacs.d/my.emacs/init.el")
-  (setq gc-cons-threshold def-threshold))
+  (setq gc-cons-threshold default))
 
 (message "emacs init time = %s" (emacs-init-time))
